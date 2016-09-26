597b28f29fa8ac50e2ce6c579a7be6a16d1a2d4c
Gihun Ham <progh2@gmail.com>
2016-09-26 11:54:57 +0900

    #8 testCreateAccount() 메소드가 예외 던지도록 함

--- src/kr/hs/emirim/progh2/bankbusiness/test/AccountTest.java
+++ src/kr/hs/emirim/progh2/bankbusiness/test/AccountTest.java
@@ -3,7 +3,7 @@ package kr.hs.emirim.progh2.bankbusiness.test;
 import kr.hs.emirim.progh2.bankbusiness.main.Account;
 
 public class AccountTest {
-	public void testCreateAccount(){
+	public void testCreateAccount() throws Exception{
 		Account account = new Account();
 		if(account == null){
 			throw new Exception("계좌생성 실패!");
