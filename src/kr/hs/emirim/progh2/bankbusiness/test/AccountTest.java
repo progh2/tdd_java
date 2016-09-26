d47adb1238018d5719115a4b0cb71e490bb29979
Gihun Ham <progh2@gmail.com>
2016-09-26 12:01:23 +0900

    #11 @Test 추가, JUnit 라이브러리 추가

--- src/kr/hs/emirim/progh2/bankbusiness/test/AccountTest.java
+++ src/kr/hs/emirim/progh2/bankbusiness/test/AccountTest.java
@@ -1,23 +1,26 @@
 package kr.hs.emirim.progh2.bankbusiness.test;
 
+import org.junit.Test;
+
 import kr.hs.emirim.progh2.bankbusiness.main.Account;
 
 public class AccountTest {
-	public void testCreateAccount() throws Exception{
+	@Test
+	public void testCreateAccount() throws Exception {
 		Account account = new Account();
-		if(account == null){
+		if (account == null) {
 			throw new Exception("계좌생성 실패!");
 		}
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		AccountTest test = new AccountTest();
 		try {
 			test.testCreateAccount();
 		} catch (Exception e) {
 			System.out.println("실패(X)");
-			e.printStackTrace();			
-			return ;
+			e.printStackTrace();
+			return;
 		}
 		System.out.println("성공(O)");
 	}
