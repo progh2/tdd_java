b3cd454ea77d92ff313d118923ab6a8daac262f9
Gihun Ham <progh2@gmail.com>
2016-09-26 11:48:41 +0900

    #5 testCreateAccount() 작성

--- src/kr/hs/emirim/progh2/bankbusiness/test/AccountTest.java
+++ src/kr/hs/emirim/progh2/bankbusiness/test/AccountTest.java
@@ -1,5 +1,10 @@
 package kr.hs.emirim.progh2.bankbusiness.test;
 
 public class AccountTest {
-
+	public void testCreateAccount(){
+		Account account = new Account();
+		if(account == null){
+			throw new Exception("계좌생성 실패!");
+		}
+	}
 }
