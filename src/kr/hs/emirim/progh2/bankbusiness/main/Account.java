5d9e23fe9de748ee4c815e9a1028d35769bb6a33
Gihun Ham <progh2@gmail.com>
2016-09-26 12:46:30 +0900

    #20 코드 리팩토링 - 변수명 수정

--- src/kr/hs/emirim/progh2/bankbusiness/main/Account.java
+++ src/kr/hs/emirim/progh2/bankbusiness/main/Account.java
@@ -3,8 +3,8 @@ package kr.hs.emirim.progh2.bankbusiness.main;
 public class Account {
 	private int mMoney;
 	
-	public Account(int i) {
-		mMoney = i;
+	public Account(int money) {
+		mMoney = money;
 	}
 
 	public int getBalance() {
