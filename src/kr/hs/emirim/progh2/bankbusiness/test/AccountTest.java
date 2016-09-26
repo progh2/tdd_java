ae64550c2ca2de4c115feaa43edaddd0cf6fa77b
Gihun Ham <progh2@gmail.com>
2016-09-26 13:30:36 +0900

    #26 코드 리팩토링 - 공통 사용되는 지역변수 account를 멤버변수로 변경하기

--- src/kr/hs/emirim/progh2/bankbusiness/test/AccountTest.java
+++ src/kr/hs/emirim/progh2/bankbusiness/test/AccountTest.java
@@ -7,15 +7,17 @@ import org.junit.Test;
 import kr.hs.emirim.progh2.bankbusiness.main.Account;
 
 public class AccountTest {
+	private Account account;
+
 	@Test
 	public void testCreateAccount() throws Exception {
-		Account account = new Account(10000);
+		account = new Account(10000);
 		assertNotEquals("계좌생성 실패!", null, account);
 	}
 
 	@Test
 	public void testGetBalance() throws Exception {
-		Account account = new Account(10000);
+		account = new Account(10000);
 		assertEquals("계좌조회 실패!", 10000, account.getBalance());
 
 		account = new Account(1000);
@@ -27,14 +29,14 @@ public class AccountTest {
 
 	@Test
 	public void testDeposit() throws Exception {
-		Account account = new Account(10000);
+		account = new Account(10000);
 		account.deposit(1000);
 		assertEquals("계좌입금 실패!", 11000, account.getBalance());
 	}
 
 	@Test
 	public void testWithdraw() throws Exception {
-		Account account = new Account(10000);
+		account = new Account(10000);
 		account.withdraw(1000);
 		assertEquals("계좌출금 실패!", 9000, account.getBalance());
 	}
